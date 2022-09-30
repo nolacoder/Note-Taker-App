75542607097b155cc4c62cc58f027fc9d2a3f3a9
Jared <jjohns3@tulane.edu>
2022-09-29 19:22:49 -0500

    completes the /notes get route

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const {readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils')
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
-app.get('/notes', (req, res) =>
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, "public/notes.html"))
 )
 
+// handle GET *
+
+app.get('/api/notes', (req, res) => {
+    res.send()
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
